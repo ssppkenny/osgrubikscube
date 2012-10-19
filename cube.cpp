fcccc3051d4859f3c35acb16e48dc5f932a73504
sergey.mikhno <sergey.mikhno@gmail.com>
2012-10-19 10:53:47 +0000

    some refactoring

--- cube.cpp
+++ cube.cpp
@@ -4,53 +4,51 @@
 using namespace std;
 
 
-int main(int argc, char** argv)
-
+void initGeode(osg::ref_ptr<osg::Geode> geode[3][3][3])
 {
-
-    osg::ref_ptr<CubeCreator> cubeCreator = new CubeCreator();
-    osg::ref_ptr<osg::Group> root = new osg::Group();
-    
-    osg::ref_ptr<osg::Geode> geode[3][3][3];
-    for (int i = 0; i<3; i++)
+  for (int i=0; i<3; i++)
     {
-      for (int j = 0; j<3; j++)
+      for (int j=0; j<3; j++)
       {
-         for (int k = 0; k<3; k++)
-     {
-       geode[i][j][k] = new osg::Geode();
-     }
+         for (int k=0; k<3; k++)
+         {
+           geode[i][j][k] = new osg::Geode();
+         }
       }
     }
 
+}
+
+void initTransforms(osg::ref_ptr<osg::MatrixTransform> transforms[3][3][3], osg::ref_ptr<CubeCreator> cubeCreator, 
+        osg::ref_ptr<osg::Group> root, osg::ref_ptr<osg::Geode> geode[3][3][3]) 
+{
+  
     float a = 0.0f;
-
-    osg::ref_ptr<osg::MatrixTransform> transforms[3][3][3];
-
-
-    for (int i = 0; i<3; i++)
+    for (int i=0; i<3; i++)
     {
-      for (int j = 0; j<3; j++)
+      for (int j=0; j<3; j++)
       {
-         for (int k = 0; k<3; k++)
-     {
-       transforms[i][j][k] = cubeCreator->createCube(geode[i][j][k], a, i,j,k);
-     }
+         for (int k=0; k<3; k++)
+         {
+            transforms[i][j][k] = cubeCreator->createCube(geode[i][j][k], a, i,j,k);
+         }
+      }
+    }
+    for (int i=0; i<3; i++)
+    {
+      for (int j= 0; j<3; j++)
+      {
+         for (int k=0; k<3; k++)
+         {
+           root->addChild( transforms[i][j][k].get());
+         }
       }
     }
 
- 
-    for (int i = 0; i<3; i++)
-    {
-      for (int j = 0; j<3; j++)
-      {
-         for (int k = 0; k<3; k++)
-     {
-       root->addChild( transforms[i][j][k].get());
-     }
-      }
-    }
+}
 
+void setLightSource(osg::ref_ptr<osg::Group> root)
+{
     osg::ref_ptr<osg::LightSource> ls = new osg::LightSource();
     ls->getLight()->setPosition(osg::Vec4(5.0f,5.0f,5.0f,0.0f));
     ls->getLight()->setAmbient(osg::Vec4(1.2,1.2,1.2,1.2));
@@ -58,9 +56,21 @@ int main(int argc, char** argv)
     ls->getLight()->setLinearAttenuation(1.0f);
     root->addChild(ls.get());
 
+}
+
+int main(int argc, char** argv)
+
+{
+    osg::ref_ptr<CubeCreator> cubeCreator = new CubeCreator();
+    osg::ref_ptr<osg::Group> root = new osg::Group();
+    
+    osg::ref_ptr<osg::Geode> geode[3][3][3];
+    initGeode(geode);
+    osg::ref_ptr<osg::MatrixTransform> transforms[3][3][3];
+    initTransforms(transforms, cubeCreator, root, geode);
+
     osg::ref_ptr<myKeyboardEventHandler> handler = new myKeyboardEventHandler(transforms); 
    
-
     osgViewer::Viewer viewer;
     viewer.setUpViewInWindow(0,0,800,600);
     viewer.addEventHandler(handler);
