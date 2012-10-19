a2a1829191e689e578b46ce09a3c2cc53e968617
sergey.mikhno <sergey.mikhno@gmail.com>
2012-10-19 08:54:39 +0000

    Initial checkin

---
+++ cubecreator.hpp
@@ -0,0 +1,22 @@
+#ifndef CUBECREATOR_H
+#define CUBECREATOR_H
+#include <osg/MatrixTransform>
+#include <osg/ref_ptr>
+#include <osg/Referenced>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/ShapeDrawable>
+
+
+
+class CubeCreator : public osg::Referenced
+{  
+   public:
+     osg::ref_ptr<osg::MatrixTransform> setTransform(osg::ref_ptr<osg::Geometry> geom , osg::ref_ptr<osg::Geode> geode, float a,  float x, float y, float z);
+     osg::ref_ptr<osg::MatrixTransform> createCube(osg::ref_ptr<osg::Geode> geode, float a, float x, float y, float z);
+  
+};
+#endif
+
+
+
