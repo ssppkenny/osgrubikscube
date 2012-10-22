e572e08ac2726803035f49fee76a442bdca5fb0d
sergey.mikhno@gmail.com <sergey.mikhno@gmail.com@81f69ef7-c2b0-bf26-8fff-66ec3b124986>
2012-10-22 10:29:14 +0000

    added big cube

--- trunk/cube.cpp
+++ trunk/cube.cpp
@@ -19,8 +19,8 @@ void initGeode(osg::ref_ptr<osg::Geode> geode[3][3][3])
 
 }
 
-void initTransforms(osg::ref_ptr<osg::MatrixTransform> transforms[3][3][3], osg::ref_ptr<CubeCreator> cubeCreator, 
-        osg::ref_ptr<osg::Group> root, osg::ref_ptr<osg::Geode> geode[3][3][3]) 
+void initTransforms(osg::ref_ptr<osg::MatrixTransform> transforms[3][3][3], osg::ref_ptr<CubeCreator> cubeCreator,
+        osg::ref_ptr<osg::Group> root, osg::ref_ptr<osg::Geode> geode[3][3][3])
 {
   
     float a = 0.0f;
@@ -57,23 +57,32 @@ void setLightSource(osg::ref_ptr<osg::Group> root)
     root->addChild(ls.get());
 
 }
-
-int main(int argc, char** argv)
-
+#ifdef linux
+int main(int argc, char **argv)
+#endif
+#ifdef WIN32
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#endif
 {
     osg::ref_ptr<CubeCreator> cubeCreator = new CubeCreator();
     osg::ref_ptr<osg::Group> root = new osg::Group();
     
+	osg::ref_ptr<osg::Geode> blackCubeGeode = new osg::Geode();
+	osg::ref_ptr<osg::MatrixTransform> blackCube = new osg::MatrixTransform();
+	blackCube = cubeCreator->createBigCube(blackCubeGeode,0,0,0,0);
+	root->addChild(blackCube);
+
+
     osg::ref_ptr<osg::Geode> geode[3][3][3];
     initGeode(geode);
     osg::ref_ptr<osg::MatrixTransform> transforms[3][3][3];
     initTransforms(transforms, cubeCreator, root, geode);
     setLightSource(root);
 
-    osg::ref_ptr<myKeyboardEventHandler> handler = new myKeyboardEventHandler(transforms); 
+    osg::ref_ptr<myKeyboardEventHandler> handler = new myKeyboardEventHandler(transforms);
    
     osgViewer::Viewer viewer;
-    viewer.setUpViewInWindow(0,0,800,600);
+    viewer.setUpViewInWindow(50,50,800,600);
     viewer.addEventHandler(handler);
     viewer.setSceneData(root.get());
     return viewer.run();
